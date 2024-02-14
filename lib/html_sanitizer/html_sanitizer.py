b0f6209560f00c32ef1783c6696bb87a8eb28723
Junanjunan <taltalrealty@gmail.com>
2024-02-14 16:52:05 +0900

    new

--- lib/html_sanitizer/html_sanitizer.py
+++ lib/html_sanitizer/html_sanitizer.py
@@ -8,6 +8,30 @@ from core.exception import AlertException
 from .allowed_dict import *
 
 
+class CustomCleaner(Cleaner):
+
+    def strip_outer_div_tag(self, html_string):
+        html_string = html_string[5:-6]
+        return html_string
+
+    def transform_result(self, result_type, result, with_tail=False):
+        if issubclass(result_type, bytes):
+            result = tostring(result, encoding='utf-8', with_tail=with_tail)
+        elif issubclass(result_type, str):
+            result = tostring(result, encoding='unicode', with_tail=with_tail)
+        return self.strip_outer_div_tag(result)
+
+    def clean_html(self, html):
+        html = f'<div>{html}</div>'
+        result_type = type(html)
+        if isinstance(html, (str, bytes)):
+            doc = fromstring(html)
+        else:
+            doc = deepcopy(html)
+        self(doc)
+        return self.transform_result(result_type, doc)
+
+
 class BaseSanitizer(metaclass=abc.ABCMeta):
     """
     SubjectSanitizer, ContentSanitizer 클래스의 추상 클래스.
@@ -21,18 +45,13 @@ class BaseSanitizer(metaclass=abc.ABCMeta):
     get_combined_filter_dict
       - 공통 허용 태그 및 속성과 개별 허용 태그 및 속성을 합칩니다.
 
-    strip_not_allowed_outer_tags
-      - lxml 라이브러리의 특성상 lxml.html.clean 메서드 결과값에 
-        div 또는 p 태그가 최상위 부모태그로 생성됩니다.
-        해당 태그를 허용 목록에 포함하지 않았다면 제거합니다.
-
     get_cleaned_data
        - 상속받은 클래스에서 오버라이딩하여 사용합니다.
          최종적으로 허용된 HTML 태그와 속성만을 반환합니다.
     """
 
     def __init__(self, is_with_library_attrs=False):
-        self.cleaner = Cleaner(safe_attrs_only=True, remove_unknown_tags=False)
+        self.cleaner = CustomCleaner(safe_attrs_only=True, remove_unknown_tags=False)
         self.base_allowed_tags_dict = deepcopy(common_allowed_tags_dict)
         self.base_allowed_attrs_dict = deepcopy(common_allowed_attrs_dict)
         if is_with_library_attrs:
@@ -46,16 +65,6 @@ class BaseSanitizer(metaclass=abc.ABCMeta):
         for key, tags in private_allowed_dict.items():
             base_allowed_dict[key] = base_allowed_dict.get(key, set()).union(tags)
         return base_allowed_dict
-    
-    def strip_not_allowed_outer_tags(self, html_tree: fromstring) -> str:
-        html_string = tostring(html_tree, encoding='unicode', method='html')
-        if html_tree.tag not in self.cleaner.allow_tags:
-            first_tag = f'<{html_tree.tag}>'
-            last_tag = f'</{html_tree.tag}>'
-            empty_string = ''
-            html_string = html_string.replace(first_tag, empty_string)
-            html_string = html_string.replace(last_tag, empty_string)
-        return html_string
 
     @abc.abstractmethod
     def get_cleaned_data(self, html_content: str):
@@ -77,8 +86,6 @@ class SubjectSanitizer(BaseSanitizer):
     
     def get_cleaned_data(self, html_content: str) -> str:
         cleaned_html = self.cleaner.clean_html(html_content)
-        html_tree = fromstring(cleaned_html)
-        cleaned_html = self.strip_not_allowed_outer_tags(html_tree)
 
         if not cleaned_html:
             raise AlertException("허용되지 않는 HTML 태그들을 변경후 제목을 다시 작성해주세요.", 400)
@@ -99,10 +106,5 @@ class ContentSanitizer(BaseSanitizer):
         self.cleaner.safe_attrs = {attr for attrs in combined_attrs.values() for attr in attrs}
 
     def get_cleaned_data(self, html_content: str) -> str:
-        if html_content:
-            cleaned_html = self.cleaner.clean_html(html_content)
-            html_tree = fromstring(cleaned_html)
-            cleaned_html = self.strip_not_allowed_outer_tags(html_tree)
-        else:
-            cleaned_html = html_content
-        return cleaned_html
+        html_content = self.cleaner.clean_html(html_content)
+        return html_content
