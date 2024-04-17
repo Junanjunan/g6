eba1bc694f2b911673069c13a057b23d86ee4376
Junanjunan <taltalrealty@gmail.com>
2024-04-17 16:34:50 +0900

    [test_refactor] Enum Class를 활용한 라우터 함수 단일화
    PYDANTIC_BASE_MODEL.model_construct
      - model_validate의 경우 선언되지 않은 타입 변수이거나, 타입이 맞지 않을 경우 오류 발생
      - model_construct의 경우 model에 선언된 타입 변수들만 필터링
        (타입이 맞지 않을 경우 오류가 발생하는지에 대해서는 추가 테스트 필요)

--- api/v1/routers/config.py
+++ api/v1/routers/config.py
@@ -1,11 +1,13 @@
 """환경설정 관련 API Router"""
-from fastapi import APIRouter, Request
+from enum import Enum
+from typing_extensions import Annotated, Union, Dict
+from fastapi import APIRouter, Request, Path
+from fastapi.encoders import jsonable_encoder
 
 from api.v1.models.config import (
     HtmlBaseResponse, MemoResponse, PolicyResponse, RegisterResponse
 )
 from api.v1.models.response import response_500
-from api.v1.models import Tags
 
 router = APIRouter(
     prefix="/config",
@@ -13,36 +15,54 @@ router = APIRouter(
 )
 
 
-@router.get("/html",
-            summary="HTML 설정 조회")
-async def read_config_html(request: Request) -> HtmlBaseResponse:
-    """HTML을 구성하는데 필요한 설정 정보를 조회합니다."""
-    return request.state.config
+class ResponseEnum(Enum):
+    HTML = {
+        "class": HtmlBaseResponse,
+        "description": "HTML을 구성하는데 필요한 설정 정보를 조회합니다."
+    }
+    POLICY = {
+        "class": PolicyResponse,
+        "description": "회원가입 약관을 조회합니다."
+    }
+    MEMBER = {
+        "class": RegisterResponse,
+        "description": "회원가입에 필요한 기본환경설정 정보를 조회합니다.<br>(회원가입 약관, 개인정보 수집 및 허용 약관)"
+    }
+    MEMO = {
+        "class": MemoResponse,
+        "description": "쪽지 발송 시, 1건당 소모되는 포인트 설정 정보를 조회합니다."
+    }
+
+    @classmethod
+    def get_type_description(cls, dict_info: dict):
+        type_description = ""
+        for key, value in dict_info.items():
+            type_description += f"- {key}: {value.annotation.__name__}\n"
+        return type_description
+
+    @classmethod
+    def config_description(cls):
+        description = """### response_type:환경설정 종류 ###\n\n"""
+        for i in cls:
+            description += f"**{i.name.lower()}**: {i.value['description']}\n\n {cls.get_type_description(i.value['class'].model_fields)}\n\n"
+        return description
 
 
-@router.get("/policy",
-            summary="회원가입 약관 조회",
-            tags=[Tags.MEMBER])
-async def read_member_policy(request: Request) -> PolicyResponse:
+@router.get("/{response_type}",
+            description=ResponseEnum.config_description(),
+            summary="환경설정 조회")
+async def read_config(
+    request: Request,
+    response_type: Annotated[str, Path(title="환경설정 종류", description="환경설정 종류")]
+) -> Dict[str, Union[str, int]]:
     """
-    회원가입 약관을 조회합니다.
-    - 회원가입 약관
-    - 개인정보 수집 및 허용 약관
+    환경설정 종류에 따라 설정 정보를 조회합니다.
+    API 문서에 적용되는 상세설명은 @router.get 데코레이터의 description에 정의되어 있습니다.
     """
-    return request.state.config
-
-
-@router.get("/member",
-            summary="회원가입 설정 조회",
-            tags=[Tags.MEMBER])
-async def read_config_member(request: Request) -> RegisterResponse:
-    """회원가입에 필요한 기본환경설정 정보를 조회합니다."""
-    return request.state.config
-
-
-@router.get("/memo",
-            summary="쪽지 발송 시, 설정 포인트 조회",
-            tags=[Tags.MEMO])
-async def read_config_memo(request: Request) -> MemoResponse:
-    """쪽지 발송 시, 1건당 소모되는 포인트 설정 정보를 조회합니다."""
-    return request.state.config
+    validate_model: ResponseEnum = getattr(ResponseEnum, response_type.upper(), None)
+    if not validate_model:
+        return {"message": "환경설정 종류가 잘못되었습니다."}
+    response_class = validate_model.value
+    config = response_class["class"].model_construct(**request.state.config.__dict__)
+    config_json = jsonable_encoder(config)
+    return config_json
